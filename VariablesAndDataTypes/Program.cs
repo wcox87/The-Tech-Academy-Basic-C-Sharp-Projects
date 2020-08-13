658552a380beafc9d27c0125bd74d3b6ead2a591
Will Cox <wcox87@Gmail.com>
2020-08-13 02:20:48 -0600

    Update
    Minor update to several projects.  No major changes

--- VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/Program.cs
@@ -16,12 +16,27 @@ namespace VariablesAndDataTypes
             //console.writeline("your name is: " + yourname);
             //console.readline();
 
-            Console.WriteLine("What is your favorite number?");
-            string favoriteNumber = Console.ReadLine();
-            int favNum = Convert.ToInt32(favoriteNumber);
-            int total = favNum + 5;
-            Console.WriteLine("Your favorite number plus 5 is: " + total);
-            Console.ReadLine();
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char firstInitial = 'X';
+            decimal moneyInBank = 100.5m;
+            double heightInCentimeteres = 145632148;
+            float secondsLeft = 2.62f;
+            short temperatureOnMars = -341;
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
         }
     }
 }
