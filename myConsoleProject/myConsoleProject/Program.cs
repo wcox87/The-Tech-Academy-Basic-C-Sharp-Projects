3b899ba9a56b06c56b5512f0d1db50bd0e9478f5
Will Cox <wcox87@Gmail.com>
2020-08-13 01:26:21 -0600

    Acme Accounting
    Add Acme Accounting Systems welcome screen

---
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
