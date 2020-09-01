86d93bb6815784b7aa79bdee319aff9995d9365d
Will Cox <wcox87@Gmail.com>
2020-09-01 01:28:39 -0600

    Add String Assignment

---
+++ Strings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Will";
+            //string quote = "The man said, \"Hello\", Jesse. \n Hello ona  new line. \n \t Hello on a tab.";
+            //string filename = @"C:\Users\Will";
+
+            //bool trueorfalse = name.Contains("l");
+            //trueorfalse = name.EndsWith("l");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(quote);
+            //Console.Read();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Will");
+
+            Console.WriteLine(sb);
+            Console.Read();
+
+            sb.Append("My name is Will");
+
+            Console.WriteLine(sb);
+            Console.Read();
+
+            sb.Append("My name is Will");
+
+            Console.WriteLine(sb);
+            Console.Read();
+        }
+    }
+}
---
+++ stringAssigment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            sb.Append(name + " enjoyed spending time outdoors and taking long walks. ");
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            sb.Append("They also had a book that was " + color + " and they very much enjoyed spending their time reading it over and over again. ");
+            
+            Console.WriteLine("What is the name of the city you live in?");
+            string city = Console.ReadLine();
+            city = city.ToUpper();
+            sb.Append("But, as the sun began to set " + name + " knew it was time to return to " + city + " which was their home. ");
+
+            
+            Console.WriteLine(name + "'s favorite color is " + color + ". And they live in " + city + ".");
+            Console.ReadLine();
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
