2f2ec7c33197c6b0d17f04f394b34d72f1f41317
Will Cox <wcox87@Gmail.com>
2020-08-31 23:47:36 -0600

    Add Package Express App

---
+++ ifStatements/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name1 = "James";
+            //string name2 = "Adam";
+            //string name3 = "Will";
+            //string name4 = "James";
+
+            //if (name4 == "James")
+            //{
+            //    Console.WriteLine("Your name is James?");
+            //    string nameCheck = Console.ReadLine();
+            //    bool nameCheck2 = Convert.ToBoolean(nameCheck);
+
+            //    if(nameCheck2 == false)
+            //    {
+            //        Console.WriteLine("What is your name?");
+            //        string userName = Console.ReadLine();
+            //        Console.WriteLine("Your name is " + userName);
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("Good.");
+            //    }
+            //}
+
+            //else if (name4 == "Adam") 
+            //{
+            //    Console.WriteLine("Your name is " + name4);
+            //}
+
+            //Console.ReadLine();
+
+            int num1 = 17;
+            int num2 = 10;
+            string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ packageExpress/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace packageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight (in Lbs): ");
+            string packageWeightString = Console.ReadLine();
+            int packageWeight = Convert.ToInt32(packageWeightString);
+
+            Console.WriteLine("Please enter the package width (in inches): ");
+            string packageWidthString = Console.ReadLine();
+            int packageWidth = Convert.ToInt32(packageWidthString);
+            
+            Console.WriteLine("Please enter the package height (in inches): ");
+            string packageHeightString = Console.ReadLine();
+            int packageHeight = Convert.ToInt32(packageHeightString);
+
+            Console.WriteLine("Please enter the package length (in inches): ");
+            string packageLengthString = Console.ReadLine();
+            int packageLength = Convert.ToInt32(packageLengthString);
+
+            float shipping = (packageHeight * packageLength * packageWidth) * packageWeight / 100;
+
+            string result = packageWeight > 50 ? "Package to big to be shipped via Package Express." : ("Your estimated total for shipping this package is: $" + shipping);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
