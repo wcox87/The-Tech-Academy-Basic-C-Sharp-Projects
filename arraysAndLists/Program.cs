fbea55c1b4a5222212af440af6b46bd3f2ac0f60
Will Cox <wcox87@Gmail.com>
2020-09-01 21:04:21 -0600

    Add arraysAndLists Assignment

---
+++ arraysAndLists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+
+        List<int> intList = new List<int>();
+
+        intList.Add(4);
+        intList.Add(10);
+
+        Console.WriteLine(intList[0]);
+        Console.Read();
+
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 5000, 600, 2300};
+
+        //int[] numArray2 = { 5, 2, 10, 5000, 600, 2300 };
+
+        //Console.WriteLine(numArray2[3]);
+        //Console.ReadLine();
+    }
+    }
---
+++ arraysAndListsAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraysAndListsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArray = new string[] { "Blue", "Red", "Yellow", "Orange", "Green" };
+
+            Console.WriteLine(strArray[2]);
+            Console.Read();
+        }
+    }
+}
