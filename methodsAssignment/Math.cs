b4146864d23e9ab8adee4aae364619db9526c067
Will Cox <wcox87@Gmail.com>
2020-09-08 13:39:46 -0600

    Add Methods Assignment

---
+++ methodsAssignment/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsAssignment
+{
+    class Math
+    {
+        public static void numberPlus(int userNum)
+        {
+            int userTotal = userNum + 10;
+            Console.WriteLine(userNum + " plus 10 equals " + userTotal);
+        }
+
+        public static void numberMultiply(int userNum)
+        {
+            int userTotal = userNum * 5;
+            Console.WriteLine(userNum + " times 5 equals " + userTotal);
+        }
+
+        public static void numberDivide(int userNum)
+        {
+            int userTotal = userNum / 2;
+            Console.WriteLine(userNum + " divided by 2 equals " + userTotal);
+        }
+    }
+}
---
+++ methodsAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to do some math!");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Math.numberPlus(userInput);
+            Math.numberDivide(userInput);
+            Math.numberMultiply(userInput);
+            Console.ReadLine();
+        }
+    }
+}
