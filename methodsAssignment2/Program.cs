ce5113381701a0a3c03b797b24e1724ba10f4c3b
Will Cox <wcox87@Gmail.com>
2020-09-11 13:43:36 -0600

    Add methodsAssignment2

---
+++ methodsAssignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass mc = new myClass();
+            Console.WriteLine("Please enter a number...");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            int userResult = mc.myMethod(userNum);
+            Console.WriteLine("The result is {0}", userResult);
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a number with a decimal...");
+            float userNum1 = float.Parse(Console.ReadLine());
+            float userResult1 = mc.myMethod(userNum1);
+            Console.WriteLine("The result is {0}", userResult1);
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter another number...");
+            string userInput = (Console.ReadLine());
+            string userResult2 = mc.myMethod(userInput);
+            Console.WriteLine("The result is " + userResult2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodsAssignment2/myClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsAssignment2
+{
+    public class myClass
+    {
+        //public int myMethod(int num1)
+        //{
+        //    int result = num1 + 50;
+        //    return result;
+        //}
+        public int myMethod(int num1)
+        {
+            int result = num1 + 50;
+            //Console.WriteLine("Result");
+            return result;
+        }
+
+        public float myMethod(float num1)
+        {
+            float result = num1 * 4;
+            //Console.WriteLine("Result");
+            return result;
+        }
+
+        public string myMethod(string num1)
+        {
+            int num2 = Convert.ToInt32(num1);
+            int result = num2 - 40;
+            string final = result.ToString();
+            //Console.WriteLine("Result");
+            return final;
+        }
+    }
+}
