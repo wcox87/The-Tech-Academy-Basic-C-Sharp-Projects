cd363514cdfd158d38f8bea3ef2211459607def8
Will Cox <wcox87@Gmail.com>
2020-09-15 15:34:44 -0600

    Add structAssignment

---
+++ structAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 92;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+
+        struct Number
+        {
+            public int Amount;
+        }
+    }
+}
