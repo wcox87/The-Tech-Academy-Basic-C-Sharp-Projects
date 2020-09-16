9f623aaf46779e813ba2843fd06043820b7c2046
Will Cox <wcox87@Gmail.com>
2020-09-15 23:05:18 -0600

    Add DateTime Assignment

---
+++ dateTimeAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date);
+            Console.WriteLine("Please enter the number of hours to add.");
+            int userHours = Convert.ToInt32(Console.ReadLine());
+            DateTime result = date.AddHours(userHours);
+            Console.WriteLine(result);
+            Console.ReadLine();
+            
+        }
+    }
+}
