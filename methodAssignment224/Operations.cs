d3c73e4802a51ab324781b0f42ad7a4be2163992
Will Cox <wcox87@Gmail.com>
2020-09-18 14:25:21 -0600

    methodAssignment
    First add

---
+++ methodAssignment224/Operations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssignment224
+{
+    public static class Operations
+    {
+        public static void Operation(double x, double y = default)
+        {
+
+            bool entered = double.TryParse(Console.ReadLine(), out y);
+            double num1 = x;
+            double num2 = y;
+            double sum = (num1 + num2) / 2;
+            Console.WriteLine(sum);
+
+        }
+        public static void Multiply(int x, int y = default)
+        {
+            bool entered = int.TryParse(Console.ReadLine(), out y);
+            int num1 = x;
+            int num2 = y;
+            int product = (num1 * num2) * 1;
+            Console.WriteLine(product);
+        }
+
+        public static void newMethod(out int age, out string firstName, out string lastName)
+        {
+            Console.WriteLine("Please type your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("And your last name please: ");
+            lastName = Console.ReadLine();
+        }
+        public static int Overload(int x, int y)
+        {
+
+            return x / y;
+        }
+        public static double Overload(double x, double y)
+        {
+
+            return x * y;
+        }
+    }
+}
---
+++ methodAssignment224/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssignment224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number and press enter.\n Then another number and press enter.,\n They will be added together then divided by two: ");
+            Operations.Operation(Convert.ToDouble(Console.ReadLine()));
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number and press enter.\n Then another number and press enter.,\n They will be multiplied together\n ");
+            Operations.Multiply(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+            int age1;
+            string firstName1;
+            string lastName1;
+            Operations.newMethod(out age1, out firstName1, out lastName1);
+            Console.WriteLine(age1);
+            Console.WriteLine(firstName1);
+            Console.WriteLine(lastName1);
+            Console.WriteLine("Type a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type another number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sum1;
+            sum1 = Operations.Overload(x, y);
+            Console.WriteLine("Your numbers divided: " + sum1);
+            Console.WriteLine("Type a number please: ");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type another number: ");
+            double b = Convert.ToInt32(Console.ReadLine());
+            double sum2;
+            sum2 = Operations.Overload(a, b);
+            Console.WriteLine("Your numbers multiplied: " + sum2 + "\n");
+            Console.WriteLine("End of program.\n\nThanks for playing! ");
+            Console.ReadLine();
+        }
+    }
+}
