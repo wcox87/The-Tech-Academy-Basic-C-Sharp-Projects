6948504e18c6d9d52379fd7fe471f4d2b72fd187
Will Cox <wcox87@Gmail.com>
2020-09-21 20:38:31 -0600

    Add Assignment 248

---
+++ Assignment248/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment248
+{
+    public class Employee<T>
+    {
+        public void Add(T input) { }
+        public List<T> things { get; set; }
+    }
+}
---
+++ Assignment248/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment248
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            Employee<int> employee2 = new Employee<int>();
+            employee.things = new List<string>() { "Will", "Adam", "James" };
+            employee2.things = new List<int>() { 100, 88, 9977 };
+
+            foreach (var thing in employee.things)
+            {
+                Console.WriteLine(thing);
+            }
+            foreach (var thing1 in employee2.things)
+            {
+                Console.WriteLine(thing1);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
