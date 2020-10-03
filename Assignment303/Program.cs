525bcf8e24a36ecf08803c886700eb8e97d55228
Will Cox <wcox87@Gmail.com>
2020-10-02 23:44:16 -0600

    Add assignment 303

---
+++ Assignment303/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string welcomeStatement = "welcome to new employee registration.";
+            Console.WriteLine("Hello, {0}", welcomeStatement);
+            Console.WriteLine("Please enter your name, address, and your salary. If you haven't been given a different salary, you will be starting at the base salary of 40,000$");
+            var userInput = Convert.ToString(Console.ReadLine());
+
+            var employee = new Employee(userInput);
+        }
+    }
+}
